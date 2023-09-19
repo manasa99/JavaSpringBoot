c6f11bfd5eee174bbd23d2b987d7e76d20e4a0c8
IManasa9 <sivatur@iu.edu>
2023-09-19 12:21:27 -0400

    exceptions

--- src/main/java/com/productCatalogService/javaspring/service/ProductService.java
+++ src/main/java/com/productCatalogService/javaspring/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.productCatalogService.javaspring.service;
 
 import com.productCatalogService.javaspring.entity.ProductEntity;
+import com.productCatalogService.javaspring.exception.ProductNotFoundException;
+import com.productCatalogService.javaspring.exception.ServerException;
 import com.productCatalogService.javaspring.model.Product;
 import com.productCatalogService.javaspring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -31,12 +34,18 @@ public class ProductService {
     public  List<Product>  getProducts(String product){
         List<ProductEntity> productEntities = new ArrayList<>();
         productEntities.addAll(productRepository.findByProductContainingIgnoreCase(product));
+        if(productEntities.isEmpty()) {
+            throw new ProductNotFoundException("No record found for " + product, HttpStatus.NOT_FOUND);
+        }
         return mapEntityToModel(productEntities);
     }
 
     public List<Product> getProductsByPrice(Double price){
         List<ProductEntity> productEntities = new ArrayList<>();
         productEntities.addAll(productRepository.findByPrice(price));
+        if(productEntities.isEmpty()) {
+            throw new ProductNotFoundException("No record found for price" + price, HttpStatus.NOT_FOUND);
+        }
         return mapEntityToModel(productEntities);
     }
 
